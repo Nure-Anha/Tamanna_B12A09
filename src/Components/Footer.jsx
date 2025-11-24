5db6a59a5b6a9412ffcba4c87e1b7d252c3efdac
Nure Anha Tamanna <nureanha99@gmail.com>
2025-11-24 19:01:24 +0600

    Navbar , Slider , Footer complete with responsiveness

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,51 @@
+import { faFacebook, faXTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h3>fooooo</h3>
-        </div>
+        <footer className="footer sm:footer-horizontal bg-[#d5d7da] text-base-content p-10">
+            <div>
+                <h3 className='font-bold text-5xl' >KiddoLand</h3>
+                <p className='text-[16px]   mt-3'>
+                    At Kiddoland, we believe every child deserves <br /> a world full of wonder. Our toys are designed <br /> to spark creativity, laughter, and <br /> magical moments for kids of all ages.
+                </p>
+                <p className=' font-medium mt-5'>
+                    Email: <a href="">hello@kiddoland.com</a> <br />
+                    Call: <a href="">(+880) 1914463784</a>
+                </p>
+            </div>
+
+
+           <nav>
+                <h6 className="footer_title_heading">Account</h6>
+                <a className='hover:text-green-400' href="">Your Account</a>
+                <a className='hover:text-green-400' href="">Ordering Tracking</a>
+                <a className='hover:text-green-400' href="">Delivery Information</a>
+                <a className='hover:text-green-400' href="">Discount</a>
+                <a className='hover:text-green-400' href="">Custom Service</a>
+                
+           </nav>
+
+           <nav>
+                <h6 className="footer_title_heading">Services</h6>
+                <a className='hover:text-green-400' href="">Sitemap</a>
+                <a className='hover:text-green-400' href="">Advanced Search</a>
+                <a className='hover:text-green-400' href="">Terms & Conditions</a>
+                <a className='hover:text-green-400' href="">Contact Us</a>
+                <a className='hover:text-green-400' href="">Privacy policy</a>
+           </nav>
+
+
+  <nav>
+    <h6 className="footer_title_heading">Social</h6>
+    <div className="grid grid-flow-col gap-4">
+      <a className='text-4xl' href=""><FontAwesomeIcon icon={faXTwitter}></FontAwesomeIcon></a>
+      <a className='text-4xl hover:text-red-600' href=""><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a>
+      <a className='text-4xl hover:text-blue-600' href=""><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon></a>
+    </div>
+  </nav>
+</footer>
     );
 };
 
---
+++ src/Components/Slider.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+const Slider = () => {
+    return (
+        <>
+      <Swiper
+        pagination={{
+          type: 'fraction',
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper text-white "
+      >
+        <SwiperSlide>
+            <div>
+            <img className='w-full h-[500px] object-cover brightness-50' src="/assets/img6.jpg" alt="" />
+
+            <div className='absolute inset-0 text-white flex flex-col justify-center items-center'>
+              <h6 className='font-medium text-xl'>Fun & Learning</h6> <br />
+              <p className='text-6xl font-bold text-center'>Early Learning Starts <br /> with the Right Toys</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div>
+            <img className='w-full h-[500px] object-cover brightness-50' src="/assets/img2.jpg" alt="" />
+
+            <div className='absolute inset-0 text-white flex flex-col justify-center items-center'>
+              <h6 className='font-medium text-xl'>Fun & Learning</h6> <br />
+              <p className='text-6xl font-bold text-center'>Let Them Dream Big -<br />Toys That Build Imagination</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div>
+            <img className='w-full h-[500px] object-cover brightness-50' src="/assets/img3.jpg" alt="" />
+
+            <div className='absolute inset-0 text-white flex flex-col justify-center items-center'>
+              <h6 className='font-medium text-xl'>Fun & Learning</h6> <br />
+              <p className='text-6xl font-bold text-center'>Unleash Their Imagination -<br /> Creative Toys for Curious Minds</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+      </Swiper>
+    </>
+    );
+};
+
+export default Slider;
